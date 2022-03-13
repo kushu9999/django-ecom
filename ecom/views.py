1a2dd5202071c0de45eddfbb956dadb0c172bfbc
Kushal Dulani <kushal@Kushals-MacBook.local>
2022-03-13 19:29:46 +0530

    product in homepage

--- ecom/views.py
+++ ecom/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from store.models import Product
 def home(request):
-    return render(request,'home.html')
+    # we are fetching products from db to show in home page
+    products = Product.objects.all().filter(is_available = True)
+    
+    context = {
+        'products' : products,
+    }
+    return render(request,'home.html', context)
