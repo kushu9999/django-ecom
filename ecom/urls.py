8ca22810df7b53d2eb2a8cbe625251f47a9af0c7
Kushal Dulani <kushal@Kushals-MacBook.local>
2022-03-14 12:29:59 +0530

    upto folder 4

--- ecom/urls.py
+++ ecom/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 # here we imported static and settings for setup image in category
@@ -24,7 +24,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('store/', include('store.urls')), # it'll redirect website.com/store to store's url,py file
 ] 
 # for cat_image 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from store.models import Product
+from category.models import Category
 
 # Create your views here.
+
+def store(request, category_slug=None):
+    categories = None
+    products = None
+    
+    if category_slug != None :
+        # it'll get obejct from category, is there not any objects it'll give 404 not found
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available = True)
+        product_count = products.count()
+    else :
+        products = Product.objects.all().filter(is_available = True)
+        product_count = products.count()
+    
+    context = {
+        'products' : products,
+        'product_count' : product_count,
+    }
+    return render(request, 'store/store.html' , context)
