f8533b58ba7dec4bb801a0c2667693ffd18cb633
kushu9999 <kushu9999@gmail.com>
2022-12-11 17:45:46 +0530

    Products added

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from store.models import Product
+
 def home(request):
-    return render(request, 'home.html')
+    product = Product.objects.all().filter(is_available=True)
+    context = {'products': product}
+    return render(request, 'home.html', context)
---
+++ store/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from store.models import Product
+from category.models import Category
+
+# Create your views here.
+def store(request, category_slug=None):
+    category = None
+    products = None
+
+    if category_slug != None :
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+    else:
+        products = Product.objects.all().filter(is_available=True)
+    context = {'products': products}
+    return render(request,'store/store.html', context)
