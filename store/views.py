9b84b7986a19778a5ad96dd0150da05b31e94dc0
kushal <kushal@darwindigital.com>
2023-01-03 18:25:52 +0530

    fixing carts

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from store.models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -10,6 +12,7 @@ def store(request, category_slug=None):
     if category_slug != None :
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        
     else:
         products = Product.objects.all().filter(is_available=True)
     context = {'products': products}
@@ -18,7 +21,8 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug = category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
-    context = {'single_product': single_product}
+    context = {'single_product': single_product, 'in_cart': in_cart}
     return render(request,'store/product_detail.html',context)
